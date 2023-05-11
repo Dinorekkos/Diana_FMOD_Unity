e3b02cf89bd72d856facdffb337d27868821546a
Diana/Dinorekkos <diana_ramos_g@hotmail.com>
2023-05-11 00:39:58 -0600

    mas o menos salto help

---
+++ Test_PM/Assets/Dino/Code/GroundChecker.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundChecker : MonoBehaviour
+{
+    
+    public Action OnGrounded;
+
+    // private void OnCollisionEnter(Collision collision)
+    // {
+    //     if (collision.gameObject.CompareTag("Ground"))
+    //     {
+    //         Debug.Log("Grounded");
+    //         OnGrounded?.Invoke();
+    //     }
+    // }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Ground"))
+        {
+            Debug.Log("Grounded");
+            OnGrounded?.Invoke();
+        }
+        
+    }
+
+}
--- Test_PM/Assets/Dino/Code/Kar_Flying.cs
+++ Test_PM/Assets/Dino/Code/Kar_Flying.cs
@@ -8,43 +8,69 @@ public class Kar_Flying : MonoBehaviour
     [SerializeField] private GameObject parachute;
     [SerializeField] private Vector3 maxScale;
     [SerializeField] private Rigidbody rb;
-    [SerializeField] private float jumpForce;
-    [SerializeField] private float jumpDuration;
-    [SerializeField] private float jumpHeight;
-    [SerializeField] private float flyingMass;
+    [SerializeField] private float jumpForce = 500;
+    [SerializeField] private GameObject groundCheck;
+    [SerializeField] private GroundChecker checker;
+    
 
-    private float _normalMass;
+    private float _normalMass = 250;
     private bool _isFlying = false;
+    private bool _isGrounded = false;
     void Start()
     {
-        _normalMass = rb.mass;
         maxScale = transform.localScale;
-        parachute.SetActive(false);
+        parachute.transform.localScale = Vector3.zero;
+        checker.OnGrounded += OnPlayerStopFlying;
+
     }
 
     void Update()
     {
-        if (_isFlying) return;
         
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (groundCheck.transform.position.y > 1)
         {
-            PlayerStartFlying();
+            _isGrounded = false;
         }
+        else
+        {
+            _isGrounded = true;
+        }
+
+        if (!_isFlying && _isGrounded)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                PlayerStartFlying();
+            }
+        }
+        
     }
 
     private void PlayerStartFlying()
     {
         _isFlying = true;
-        parachute.transform.localScale = Vector3.zero;
-        parachute.SetActive(true);
         parachute.transform.DOScale(maxScale, 1.5f).SetEase(Ease.OutBounce);
-        transform.DOJump(transform.position + Vector3.one, jumpHeight, 1, jumpDuration);
+        DoJump();
     }
     
     private void OnPlayerStopFlying()
     {
-        rb.mass = flyingMass;
-        parachute.transform.DOScale(Vector3.zero, 1.5f).SetEase(Ease.InBounce);
+        if (_isFlying)
+        {
+            Debug.Log("OnPlayerStopFlying");
+             rb.mass = 250f;
+             Debug.Log("rb.mass = " + rb.mass);
+             parachute.transform.DOScale(Vector3.zero, 1.5f).SetEase(Ease.InBounce);
+            _isFlying = false;
+
+        }
+    }
+
+    private void DoJump()
+    {
+        rb.mass = 60f;
+        rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        
     }
     
     
